549c651484f89f82e4afef7aafcc94fa57d4e7d9
abhishek <abhishek@innovatia.co.in>
2022-08-28 13:51:38 +0530

    change branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React  from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {React , useEffect} from 'react';
+import AOS from 'aos';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/header';
+import Footer from './components/footer';
+import '../src/assets/font/font-awesome.css'
+import routes from './pages';
+import Page404 from './pages/404';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        AOS.init({
+          duration : 2000
+        }); 
+      }, []);
+
+    return (
+        <>
+
+            <Header />
+
+            <Routes>
+
+                {
+                    routes.map((data,idx) => (
+                        <Route key={idx} path={data.path} element={data.component} exact />
+                    ))
+                }
+
+                <Route path='*' element={<Page404 />} />
+            </Routes>
+
+            <Footer />
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import './App.scss'
+import ScrollToTop from "./ScrollToTop";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.Fragment>
+        <BrowserRouter>
+        <ScrollToTop />
+            <App />
+        </BrowserRouter>
+    </React.Fragment>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
